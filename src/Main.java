a1f66a308cc46e49e1af2549d306223a219ac938
PupilCurious <ertugrul.bilal78@gmail.com>
2023-02-23 22:43:38 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        int birthYear;
+        boolean isValid=true;
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Doğum Yılınızı Giriniz : ");
+        birthYear = input.nextInt();
+        if(birthYear<=0){
+            isValid = false;
+        }
+        if(isValid){
+            if(birthYear % 12 == 0){
+                System.out.println("Maymun");
+            } else if(birthYear % 12 == 1){
+                System.out.println("Horoz");
+            } else if (birthYear % 12 == 2) {
+                System.out.println("Köpek");
+            } else if (birthYear % 12 == 3) {
+                System.out.println("Domuz");
+            } else if (birthYear % 12 == 4) {
+                System.out.println("Fare");
+            }  else if(birthYear % 12 == 5){
+                System.out.println("Öküz");
+            } else if (birthYear % 12 == 6) {
+                System.out.println("Kaplan");
+            } else if (birthYear % 12 == 7) {
+                System.out.println("Tavşan");
+            } else if (birthYear % 12 == 8) {
+                System.out.println("Ejderha");
+            }  else if(birthYear % 12 == 9){
+                System.out.println("Yılan");
+            } else if (birthYear % 12 == 10) {
+                System.out.println("At");
+            } else if (birthYear % 12 == 11) {
+                System.out.println("Koyun");
+        }
+            else{
+            System.out.println("0 'dan farklı bir yıl giriniz");
+        }
+
+    }
+}}
